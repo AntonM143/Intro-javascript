96b34e1d214e92ed2f637fe198c5852d4573bc54
Anton Månsson <anton.mansson@mediainstitutet.se>
2020-10-01 17:08:20 +0200

    prog

--- logic.js
+++ logic.js
@@ -1,14 +1,49 @@
-/* var name = "Anton";
+
+/*1 var name = "Anton";
 age = "23 år";
 favopet = "dog"
 
 console.log(name, age, favopet) */
 
 
+ /* let answer = prompt("skriv din favo färg")
+console.log(answer)
+alert(answer)
+ */
+
+ /*3 let NumberOne = Number(prompt("Ange första siffran"))
+ let NumberTwo = Number(prompt("Ange andra siffran"))
+ let method = prompt("Ange + - ")
+ let sum 
+
+ if(method == "+") {
+     sum = NumberOne + NumberTwo 
+    } else if(method == "-") {
+    sum = NumberOne - NumberTwo }
+    console.log(sum)
+    alert(sum) */
+/* 4
+let favColor = prompt("Din Favoritfärg:")
+
+function logcolor() {
+    console.log(favColor)
+} */
+/* 
+logcolor("Blue", "Green")
+logcolor("Yellow", "Red")
+
+console.log("Blue", "Green")
+console.log("Yellow", "Red") */
+/* const logbutton = document.getElementById("logbutton")
+logbutton.addEventListener("click", logcolor)
+
+function logcolor() {
+console.log (logbutton)
+} */
+
+let firstName = prompt("Ange ditt förnamn")
+let lastName = prompt("Ange ditt efternamn")
 
 
-
-
-function myfunction () {
-    var = blue
-}
+console.log(firstName, lastName)
+alert(firstName, lastName);

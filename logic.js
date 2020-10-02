156bb7d1669a048d0fe141b5d13c2825505e2d37
Anton Månsson <anton.mansson@mediainstitutet.se>
2020-10-02 17:21:03 +0200

    prog

--- logic.js
+++ logic.js
@@ -6,10 +6,10 @@ favopet = "dog"
 console.log(name, age, favopet) */
 
 
- /* let answer = prompt("skriv din favo färg")
+/*  let answer = prompt("skriv din favo färg")
 console.log(answer)
-alert(answer)
- */
+alert(answer) */
+
 
  /*3 let NumberOne = Number(prompt("Ange första siffran"))
  let NumberTwo = Number(prompt("Ange andra siffran"))
@@ -22,18 +22,63 @@ alert(answer)
     sum = NumberOne - NumberTwo }
     console.log(sum)
     alert(sum) */
-/* 4
-let favColor = prompt("Din Favoritfärg:")
+//4
+/* let favColor = prompt("Din Favoritfärg:")
 
 function logcolor() {
     console.log(favColor)
-} */
-/* 
+} 
+
 logcolor("Blue", "Green")
 logcolor("Yellow", "Red")
 
 console.log("Blue", "Green")
-console.log("Yellow", "Red") */
+console.log("Yellow", "Red")  */
+
+
+/* const onclick = document.getElementById("onclick")
+document.getElementById("onclick")
+
+let text = prompt("skriv hej:")
+function changetext (change){
+    
+    change.innertext = text;
+    console.log(changetext)
+} */
+//vidareuppgift 1
+/* const el = document.getElementById("myTitle"); 
+  el.addEventListener("click", changeTitle);
+  let title = prompt("Change the title:"); 
+  el.innerText = title
+function changeTitle() {
+    
+      console.log(title)
+     
+  }             */
+  const cc= document.getElementById("div");
+  const cc1= document.getElementById("div1");
+  cc.addEventListener("click", changeap);
+  cc1.addEventListener("click", changeap);
+  cc.style.width = "200px"
+  cc.style.height = "200px"
+  cc.style.background = "blue"
+  cc1.style.background = "green"
+  cc1.style.width = "200px"
+  cc1.style.height = "200px"
+  function changeap(){
+      changeap("blue", "green")
+      changeap("yellow", "red")
+      console.log("blue", "green")
+      console.log("yellow", "red")
+      
+      
+  }
+  if("changeap")
+  
+
+
+
+
 /* const logbutton = document.getElementById("logbutton")
 logbutton.addEventListener("click", logcolor)
 
@@ -41,9 +86,53 @@ function logcolor() {
 console.log (logbutton)
 } */
 
-let firstName = prompt("Ange ditt förnamn")
+/* let firstName = prompt("Ange ditt förnamn")
 let lastName = prompt("Ange ditt efternamn")
+let sum
+if (sum == "+") {
+    sum = firstName + lastName
+    
+}
+
+console.log(sum)
+
+alert(sum)  */
 
 
-console.log(firstName, lastName)
-alert(firstName, lastName);
+/* let fullName = prompt("Ange ditt förnamn & efternman")
+console.log(fullName)
+alert(fullName) */
+
+/* const logbutton = document.getElementById("logbutton")
+logbutton.addEventListener("click", onclick)
+
+function onclick() {
+    let colordiv = document.getElementById("colordiv")
+    let weo = document.getElementById("weo")
+}
+if (colordiv.style.backgroundColor == "red") {
+        colordiv.style.backgroundColor = "green"
+        weo.innerText = "green"
+}else if (colordiv.style.backgroundColor == "yellow"){
+        colordiv.style.backgroundColor = "blue" 
+        weo.innerText = "blue"
+}else {
+        colordiv.style.backgroundColor ="blue"
+        weo.innerText = "blue"
+}     
+         */
+    
+  
+
+
+
+
+//5
+/* var firstName = prompt("Ange förnamn")
+var lastName = prompt("Ange efternamn")
+console.log(firstName , lastName)
+ alert(firstName + " " + lastName)  */
+
+
+ 
+ 
